e0708127a14b0f2f69f2bed09710e22bf73230f5
uoy-fangyan <95178513+uoy-fangyan@users.noreply.github.com>
2023-02-13 18:05:32 +0000

    Add files via upload

--- ResourceManager.thy
+++ ResourceManager.thy
@@ -4,7 +4,7 @@ theory ResourceManager
   imports "Z_Machines.Z_Machine"
 begin 
 
-type_synonym resource = integer
+type_synonym resource = int
 consts Resource :: "resource set"
 
 \<comment>\<open>A resource manager allocates identical but numbered resources to client programs or users.
@@ -12,13 +12,6 @@ Using a set of numbers @free to describe the free resources\<close>
 
 zstore ResourceManager =
    free :: "resource set"
-
- (*  
-  free :: "integer set"
-  res  :: "integer set"
-  where "free \<subseteq> res" "res \<subseteq> RES"
-  why is res needed? ? ? 
-*)
 
 \<comment>\<open>Any resource that is currently free may be allocated.If there is more than one resource free, then this specification is nondeterministic.\<close>
 zoperation Allocate =
@@ -28,12 +21,13 @@ zoperation Allocate =
   update "[free\<Zprime> = free - {r}]"
 
 \<comment>\<open>a refined version of Allocate: should there be more than one resource free, the resource with the lowest number should be allocated first. \<close>
-zoperation Allocate\<^sub>1 =
+zoperation Allocate1 =
   over ResourceManager
   params r \<in> Resource
-  pre "r \<in> free \<and> r = Min free \<and> free\<noteq>{}"
+  pre "r \<in> free \<and> r = Min free "
   update "[free\<Zprime> = free - {r}]"
-(*is free\<noteq>{} necessary? ? ? *)
+(* free\<noteq>{} is not necessary in precondition as 'r \<in> free' implies it
+   but for animation, 'r \<in> free' must be before 'r = Min free'*)
 
 zoperation Deallocate =
   over ResourceManager
@@ -41,19 +35,18 @@ zoperation Deallocate =
   pre "r \<notin> free"
   update "[free\<Zprime> = free \<union> {r}]"
 
-lemma Allocate\<^sub>1_refines_Allocate: "Allocate r \<sqsubseteq> Allocate\<^sub>1 r"
-  unfolding Allocate_def Allocate\<^sub>1_def by refine
-(*data refinement proof ? ? ?*)
+lemma Allocate\<^sub>1_refines_Allocate: "Allocate r \<sqsubseteq>\<^sub>\<D> Allocate1 r"
+  unfolding Allocate_def Allocate1_def oops
+
 
 
 zmachine ResourceManagerProc =
   over ResourceManager
-  init "[ free\<Zprime> = {}]"
-  operations Allocate\<^sub>1  Deallocate
+  init "[ free\<Zprime> = {0..5}]"
+  operations Allocate1  Deallocate
 
-def_consts Resource = "{0,1,2,3,4,5}"
-(*
-def_consts Resource = "set (map integer_of_int [0..5])"*)
+def_consts Resource = "{0..5}"
+
 
 animate ResourceManagerProc
 
